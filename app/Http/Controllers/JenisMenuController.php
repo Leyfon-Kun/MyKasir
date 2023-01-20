8315b171910aef05c1f212e9f02a4ef9d5f9503b
Leyfon-Kun <uzumakibimbim073@gmail.com>
2023-01-20 08:04:37 +0700

    update model/controller

---
+++ app/Http/Controllers/JenisMenuController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JenisMenu;
+use Illuminate\Http\Request;
+
+class JenisMenuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JenisMenu  $jenisMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JenisMenu $jenisMenu)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JenisMenu  $jenisMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JenisMenu $jenisMenu)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JenisMenu  $jenisMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JenisMenu $jenisMenu)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JenisMenu  $jenisMenu
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JenisMenu $jenisMenu)
+    {
+        //
+    }
+}
